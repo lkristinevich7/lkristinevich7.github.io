e0c3e69dc38d03eb1a4af7c01d37d14a219403bb
Liudmila Krystsinevich <lkristinevich7@gmail.com>
2019-03-13 12:28:15 +0100

    poprawione

--- zad.dom ajaxJQuery/js/main.js
+++ zad.dom ajaxJQuery/js/main.js
@@ -2,16 +2,20 @@ $(document).ready(function(){
 
     function getUserData() {
 
-    $('#button').click(function(){
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(jsonUserData){
-        console.log(jsonUserData); });
+        console.log(jsonUserData); 
+
+        $('#button').click(function(){
+     
 
         $('button').after('<div></div>');
         $('div').attr('id', 'dane-programisty');
-        $('div').text(jsonUserData);
-    });
+        $('div').text('Imie: '+ jsonUserData.imie +', Nazwisko: '+ jsonUserData.nazwisko +', Firma: '+ jsonUserData.firma +', Zawod: '+ jsonUserData.zawod);
+        });
 
-    }
+        });
+
+    };
 
     getUserData();
 
