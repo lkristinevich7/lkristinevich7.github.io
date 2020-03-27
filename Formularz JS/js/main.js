b927d3ae857de1b9cea1c935fe82398b82345a08
Liudmila Krystsinevich <lkristinevich.webdev@gmail.com>
2020-03-27 09:48:09 +0100

    commit

--- Formularz JS/js/main.js
+++ Formularz JS/js/main.js
@@ -1,14 +1,11 @@
-
-
-
-function validateForm(e){
+function validateForm(e) {
     const nameInput = document.getElementById('name');
     const emailInput = document.getElementById('email');
     const telInput = document.getElementById('tel');
 
     const location = document.getElementById('city');
     const invalid = location.value == "MIASTO";
-        
+
 
     const agreementCheckbox = document.getElementById('first_agreement');
     const message = document.getElementById('message');
@@ -17,85 +14,57 @@ function validateForm(e){
     message.innerHTML = '';
 
 
-    if(nameInput.value === '') {
-    e.preventDefault();
-    const msgName = document.createElement('li');
-    msgName.innerText = 'Wprowadz imie!';
-    message.appendChild(msgName);
-    nameInput.className = 'error';
+    if (nameInput.value === '') {
+        e.preventDefault();
+        const msgName = document.createElement('li');
+        msgName.innerText = 'Wprowadz imie!';
+        message.appendChild(msgName);
+        nameInput.className = 'error';
     }
 
-    if(emailInput.value === ''){
-    e.preventDefault();
-    const msgEmail = document.createElement('li');
-    msgEmail.innerText = 'Wprowadz email';
-    message.appendChild(msgEmail);
-    emailInput.className = 'error';
+    if (emailInput.value === '') {
+        e.preventDefault();
+        const msgEmail = document.createElement('li');
+        msgEmail.innerText = 'Wprowadz email';
+        message.appendChild(msgEmail);
+        emailInput.className = 'error';
     }
 
-    if(telInput.value === ''){
-    e.preventDefault();
-    const msgTel = document.createElement('li');
-    msgTel.innerText = 'Wprowadz numer telefonu!';
-    message.appendChild(msgTel);
-    telInput.className = 'error';
+    if (telInput.value === '') {
+        e.preventDefault();
+        const msgTel = document.createElement('li');
+        msgTel.innerText = 'Wprowadz numer telefonu!';
+        message.appendChild(msgTel);
+        telInput.className = 'error';
     }
 
-    if(!agreementCheckbox.checked){   
-    e.preventDefault();
-    const msgAgrement = document.createElement('li');
-    msgAgrement.innerText = 'Zaznacz zgode!';
-    message.appendChild(msgAgrement);
+    if (!agreementCheckbox.checked) {
+        e.preventDefault();
+        const msgAgrement = document.createElement('li');
+        msgAgrement.innerText = 'Zaznacz zgode!';
+        message.appendChild(msgAgrement);
     }
 
     if (invalid) {
-    const msgSelect = document.createElement('li');
-    msgSelect.innerText = 'Wybierz maisto';
-    message.appendChild(msgSelect);
-    location.className = 'error';
-    e.preventDefault();
-    }
-    else {
+        const msgSelect = document.createElement('li');
+        msgSelect.innerText = 'Wybierz maisto';
+        message.appendChild(msgSelect);
+        location.className = 'error';
+        e.preventDefault();
+    } else {
         location.className = '';
     }
-    
+
     return !invalid;
 
 }
 
 document.querySelector('form').addEventListener('submit', validateForm);
-
 document.querySelector('select').addEventListener('change', validateForm);
 
 
-
-// 
-
-// var status = "less";
-
-// function toggleText()
-// {
-//     var text="Here is some text that I want added to the HTML file";
-
-//     if (status == "less") {
-//         document.getElementById("first_agreement").innerHTML=text;
-//         document.getElementById("toggleButton").innerText = "See Less";
-//         status = "more";
-//     } else if (status == "more") {
-//         document.getElementById("first_agreement").innerHTML = "";
-//         document.getElementById("toggleButton").innerText = "See More";
-//         status = "less"
-//     }
-// }
-
-document.querySelector("a").addEventListener("click", function(e){
+document.querySelector("a").addEventListener("click", function (e) {
     e.preventDefault();
     document.querySelector("a").remove();
     document.getElementById("label1").innerHTML += "Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit";
 });
-
-
-
-
-
-
